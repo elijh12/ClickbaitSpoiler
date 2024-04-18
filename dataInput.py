d9ea945771ee59cc4c1f71da3dff74a312a97273
NChidambaram06 <chidamnm@mail.uc.edu>
2024-04-17 21:05:52 -0400

    add phrase summarization

--- dataInput.py
+++ dataInput.py
@@ -168,6 +168,71 @@ def summarizeMulti(targetParagraphs):
     return summary
 
 
+def summarizePhrase(targetParagraphs, phraseIndicators):
+    text = ""
+    for i in targetParagraphs:
+        text += i
+
+    # print(text)
+    # print("\n\n\n\n")
+
+    stopwords = list(STOP_WORDS)
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    tokens = [token.text for token in doc]
+    # print(tokens)
+
+    punctuation = string.punctuation + "\n"
+    # print(punctuation)
+
+    # print(phraseIndicators)
+
+    word_frequencies = {}
+    for word in doc:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text.lower() not in phraseIndicators:
+                    if word.text not in word_frequencies.keys():
+                        word_frequencies[word.text] = 1
+                    else:
+                        word_frequencies[word.text] += 1
+    # print(word_frequencies)
+
+    max_frequency = max(word_frequencies.values())
+    # print(max_frequency)
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word] / max_frequency
+    # print(word_frequencies)
+
+    sentence_tokens = [sent for sent in doc.sents]
+    # print(sentence_tokens)
+
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent] += word_frequencies[word.text.lower()]
+    # print(sentence_scores)
+
+    select_length = 1
+    # select_length = 2
+    # print(select_length)
+
+    summary = nlargest(select_length, sentence_scores, key=sentence_scores.get)
+    # print(summary)
+
+    final_summary = [word.text for word in summary]
+    summary = " ".join(final_summary)
+    # print(text)
+
+    # print(summary)
+    return summary
+
+
 # Function used for classifying the current document
 # currently based only on the postText field
 def categorize(thingToCheck):
@@ -195,6 +260,8 @@ if __name__ == "__main__":
 
     stopwords = list(STOP_WORDS)
 
+    phraseIndicators = []
+
     # Initialize counts
     countPassage = 0
     countPassageCorrect = 0
@@ -202,6 +269,9 @@ if __name__ == "__main__":
     countMulti = 0
     countMultiCorrect = 0
     countMultiWrong = 0
+    countPhrase = 0
+    countPhraseCorrect = 0
+    countPhraseWrong = 0
     # countMultiActual = 0
     # countMultiCounting = 0
     # countPhraseActual = 0
@@ -277,14 +347,15 @@ if __name__ == "__main__":
 
         if trainingData[i]["tags"][0] == "multi":
             countMulti += 1
-            if countMulti == 20:
+            if countMulti > 100:
+                countMulti -= 1
                 break
             print("countMulti: ", countMulti)
             text = trainingData[i]["targetParagraphs"]
             # for j in text:
             #     j.replace('"', "'")
             # # print(text)
-            summary = summarizePassage(text)
+            summary = summarizeMulti(text)
             checkKey = 0
             if trainingData[i]["targetKeywords"] == None:
                 givenSummary = (
@@ -333,6 +404,70 @@ if __name__ == "__main__":
                     # print(trainingData[i]["spoiler"][0])
                     # break
 
+        # if trainingData[i]["tags"][0] == "phrase":
+        #     countPhrase += 1
+        #     if countPhrase > 100:
+        #     countPhrase -= 1
+        #         break
+        #     print("countPhrase: ", countPhrase)
+        #     text = trainingData[i]["targetParagraphs"]
+        #     # for j in text:
+        #     #     j.replace('"', "'")
+        #     # # print(text)
+        #     for word in trainingData[i]["postText"]:
+        #         if word not in stopwords:
+        #             phraseIndicators.append(word)
+        #     checkKey = 0
+        #     if trainingData[i]["targetKeywords"] == None:
+        #         summary = summarizePhrase(text, phraseIndicators)
+        #         givenSummary = trainingData[i]["spoiler"][0].strip(",").split(" ")
+        #         for j in givenSummary:
+        #             if j not in stopwords:
+        #                 if j in summary:
+        #                     checkKey += 1
+        #         if (
+        #             checkKey
+        #             / len(
+        #                 trainingData[i]["spoiler"][0].strip(".").strip(",").split(" ")
+        #             )
+        #             >= 0.5
+        #         ):
+        #             countPhraseCorrect += 1
+        #         else:
+        #             countPhraseWrong += 1
+        #             # print(summary)
+        #             # print(trainingData[i]["spoiler"][0])
+        #             # break
+        #     else:
+        #         # print(
+        #         #     trainingData[i]["targetKeywords"].strip(" ").strip(".").split(",")
+        #         # )
+        #         for word in trainingData[i]["targetKeywords"].split(","):
+        #             if word not in stopwords:
+        #                 phraseIndicators.append(word)
+        #         summary = summarizePhrase(text, phraseIndicators)
+        #         for j in (
+        #             trainingData[i]["targetKeywords"].strip(" ").strip(".").split(",")
+        #         ):
+        #             if j in summary:
+        #                 checkKey += 1
+        #         if (
+        #             checkKey
+        #             / len(
+        #                 trainingData[i]["targetKeywords"]
+        #                 .strip(" ")
+        #                 .strip(".")
+        #                 .split(",")
+        #             )
+        #             >= 0.5
+        #         ):
+        #             countPhraseCorrect += 1
+        #         else:
+        #             countPhraseWrong += 1
+        #             # print(summary)
+        #             # print(trainingData[i]["spoiler"][0])
+        #             # break
+
     """ This Section Not Being Used Right Now:
     for i in range(len(trainingData)):
         count += 1
@@ -365,6 +500,10 @@ if __name__ == "__main__":
     print(countMultiCorrect)
     print(countMultiWrong)
     print(countMultiCorrect / countMulti)
+
+    print(countPhraseCorrect)
+    print(countPhraseWrong)
+    print(countPhraseCorrect / countPhrase)
 
     # print(count)
     # print(countMultiActual)
